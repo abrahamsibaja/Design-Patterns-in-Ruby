0edc1ea2a7d6fce328359b2b2220b3359953110a
Abraham <asibaja@pernix-solutions.com>
2013-11-28 10:48:17 -0600

    first commit

---
+++ chap1.rb
@@ -0,0 +1,14 @@
+my_car = Car.new
+my_car.drive(200)
+
+if is_car
+  my_car = Car.new
+  my_car.drive(200)
+else
+  my_plane = AirPlane.new
+  my_car.fly(200)
+end
+
+my_vehicle = get_vehicle
+my_vehicle.travel(200)
+
