e3adfc2b2e668349604db56e56ab1ed50ad7a7fb
Abraham <asibaja@pernix-solutions.com>
2014-03-11 09:18:55 -0600

    Observer array created

--- Observer.rb
+++ Observer.rb
@@ -8,16 +8,30 @@ end
 class Employee
   attr_reader :name, :title, :salary
 
-  def initialize(name, title, salary, payroll)
+  def initialize(name, title, salary)
     @name = name
     @title = title
     @salary = salary
-    @payroll = payroll
+    @observers = []
   end
 
-  def salary=(salary)
-    @salary = salary
-    @payroll.update(self)
+  def salary=(new_salary)
+    @salary = new_salary
+    notify_observers
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.update(self)
+    end
+  end
+
+  def add_observer( observer )
+    @observers << observer
+  end
+
+  def delete_observer( observer )
+    @observer.delete( observer )
   end
 
   def info
@@ -25,7 +39,8 @@ class Employee
   end
 end
 
+fred = Employee.new("Fred Flintstone", "Crane Operator", 3000)
 payroll = Payroll.new
-fred = Employee.new("Fred Flintstone", "Crane Operator", 3000, payroll)
+fred.add_observer( payroll )
 fred.salary = 3500 #Giving Fred a raise
 #fred.info
