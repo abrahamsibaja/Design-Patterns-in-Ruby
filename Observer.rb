82d3575b7ac1e7ce709f376a8f675561b8547259
Abraham <asibaja@pernix-solutions.com>
2014-03-11 09:06:09 -0600

    Payroll Class created + salary method changed

--- Observer.rb
+++ Observer.rb
@@ -1,18 +1,31 @@
-class Employee
-  attr_reader :name
-  attr_accessor :title, :salary
-
-  def initialize(name, title, salary)
-    @name = name
-    @title = title
-    @salary = salary
-  end
-
-  def info
-    puts name,title,salary
+class Payroll
+  def update( changed_employee )
+    puts("Cut a new check for #{changed_employee.name}!")
+    puts("His salary is now #{changed_employee.salary}!")
   end
 end
 
-fred = Employee.new("Fred Flintstone", "Crane Operator", 3000)
+class Employee
+  attr_reader :name, :title, :salary
+
+  def initialize(name, title, salary, payroll)
+    @name = name
+    @title = title
+    @salary = salary
+    @payroll = payroll
+  end
+
+  def salary=(salary)
+    @salary = salary
+    @payroll.update(self)
+  end
+
+  def info
+    puts name, title, salary
+  end
+end
+
+payroll = Payroll.new
+fred = Employee.new("Fred Flintstone", "Crane Operator", 3000, payroll)
 fred.salary = 3500 #Giving Fred a raise
-fred.info
+#fred.info
